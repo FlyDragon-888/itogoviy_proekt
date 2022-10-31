7d2dced892d9f79079a8b3dd7c6fed8f571abdc4
Azamat <azamat-nazmutdinov@yandex.ru>
2022-10-31 08:04:18 +0500

    Proekt version3

--- Program.cs
+++ Program.cs
@@ -21,7 +21,23 @@ void PrintMatrix(string?[] matrix)
     Console.WriteLine("[{0}]", string.Join(", ", matrix));
 }
 
+string[] MatrixRowsLen3(string[] matrix)
+{
+    string[] matrix1 = new string[matrix.GetLength(0)];
+    int j = 0;
+    for(int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        if(matrix[i].Length <= 3)
+        {
+            matrix1[j] = matrix[i];
+            j += 1;
+        }
+    }
+    return matrix1.Where(x => x != null).ToArray();
+}
 
 int m = Convert.ToInt32(ReadNumber("Введите количество строк"));
 string[] matr = GetMatrix(m);
 PrintMatrix(matr);
+string[] matr1 = MatrixRowsLen3(matr);
+PrintMatrix(matr1);
