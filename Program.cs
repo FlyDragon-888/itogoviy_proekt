0905f0cfdcd95e2388bbf769ae41cc1f8347111d
Azamat <azamat-nazmutdinov@yandex.ru>
2022-10-31 07:58:38 +0500

    Proekt version1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+string[] GetMatrix(int rowsCount)
+{
+    string[] matrix = new string[rowsCount];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        matrix[i] = ReadNumber("Введите строку");
+    }
+    return matrix;
+}
+
+int m = Convert.ToInt32(ReadNumber("Введите количество строк"));
+string[] matr = GetMatrix(m);
